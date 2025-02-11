781db8ba783795c0d7aa7ac2c56449110332d6df
Arseniy Shirokov <shirokovarsx@gmail.com>
2025-02-11 21:41:21 +0300

    Add AT

--- src/test/java/Com/apiTests/TestsApiClass.java
+++ src/test/java/Com/apiTests/TestsApiClass.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import java.io.File;
 import static org.hamcrest.Matchers.is;
 
 public class TestsApiClass {
@@ -18,19 +19,50 @@ public class TestsApiClass {
     }
 
 
-
 @Test
 public void whenRequestPost() {
+    File jsonFile = new File("src/test/resources/postpets.json");
+
     String response = RestAssured
             .given()
                 .baseUri("https://petstore.swagger.io/v2")
             .header("Content-Type", "application/json")
-            .body(jsonBody)
+            .body(jsonFile)
             .when().post("/user")
-            .then().statusCode(201)
+            .then().statusCode(200)
             .extract()
             .asString();
             System.out.println(response);
+
+}
+
+@Test
+//Проверка что гет не создает пользователей
+    public void whenRequestPost2() {
+    File jsonFile = new File("src/test/resources/postpets.json");
+    String response = RestAssured
+            .given().baseUri("https://petstore.swagger.io/v2")
+            .header("Content-Type", "application/json")
+            .body(jsonFile)
+            .when().post("/store/inventory")
+            .then().statusCode(405)
+            .extract()
+            .asString();
+            System.out.println(response);
+}
+@Test
+public void whenRequestPut() {
+    File jsonFile = new File("src/test/resources/postpets.json");
+    String response = RestAssured
+            .given().baseUri("https://petstore.swagger.io/v2")
+            .header("Content-Type", "application/json")
+            .body(jsonFile)
+            .when().put("/user/9223372036854775629")
+            .then().statusCode(200)
+            .extract()
+            .asString();
+    System.out.println(response);
+
 
 }
 }
