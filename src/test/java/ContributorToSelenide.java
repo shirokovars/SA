a6534972a3a834d72c902a8bde3bf200e40076dc
Arseniy Shirokov <shirokovarsx@gmail.com>
2025-02-26 21:51:57 +0300

    fix AT for UI

--- src/test/java/ContributorToSelenide.java
+++ src/test/java/ContributorToSelenide.java
@@ -1,8 +1,10 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class ContributorToSelenide {
 
@@ -13,6 +15,9 @@ public class ContributorToSelenide {
         Configuration.browserSize = "1900x1600";
 
         open("https://github.com/selenide/selenide");
-        $("div.Layout-sidebar").$(Selectors.byText("Contributors")
+        $("div.Layout-sidebar").$(Selectors.byText("Contributors"))
+        .closest("h2").parent().$$("li").get(2).hover();
+        $$(".Popover").findBy(Condition.visible).shouldHave(Condition.text("Vinogradov"));
+
     }
 }
